95b040c3905c568c9691e0fa1fe4ca64babd7066
samuraiOrDev <vm.ordiales.garcia@gmail.com>
2024-05-01 12:22:36 +0200

    Cambio img Banner About No Bg

---
+++ src/const/data.ts
@@ -0,0 +1,42 @@
+export const textHeroSection = {
+	title:
+		"Hello I'am <span>Víctor Manuel</span> Ordiales García. <br> <span>Full Stack Developer</span>.",
+	description:
+		"Desarrollador Full Stack con experiencia en tecnologías como React, Node, Express, MongoDB, entre otras. Apasionado por la tecnología y la programación.",
+}
+
+export const skillsSection = {
+	title: "My <span>Skills</span>",
+}
+export const experienceSection = {
+	title: "My <span>Experience</span>",
+}
+export const experiences = [
+	{
+		title: "Desarrollador OWCS",
+		company: "Viewnext S.A",
+		date: "Agosto 2021 - Febrero 2023",
+		description:
+			"Realización de proyectos y evolutivos Frontend dentro del CMS Oracle Web Center Site (OWCS) para el cliente Naturgy. ",
+	},
+	{
+		title: "Desarrollador Full Stack",
+		company: "Viewnext S.A",
+		date: "Febero 2023 - Actualidad",
+		description:
+			"Relización de proyectos y evolutivos Full para el cliente Naturgy. Todo esto hacienco uso de Spring Framework para el Backend y OWCS como CMS.",
+	},
+	{
+		title: "I + D",
+		company: "eCapure3D S.L",
+		date: "Abril 2021 - Junio 2021",
+		description:
+			"Algoritmos predictivos y análisis de datos dentro el modelo 3D.",
+	},
+]
+
+export const aboutSection = {
+	title: "About <span>Me</span>",
+	description:
+		"Graduado en Física y desarrollador full stack con habilidades sólidas en JavaScript (JS), TypeScript (TS), Java y sistemas de gestión de contenido como Oracle WebCenter Sites (OWCS).<br/> <br/>Mi formación en Física me ha proporcionado una mentalidad analítica y resolutiva, mientras que mi experiencia en desarrollo me permite crear soluciones técnicas eficientes y escalables. <br/> <br/>He trabajado en proyectos desafiantes que requieren un enfoque meticuloso y estratégico para la implementación de sistemas de gestión de contenido que optimicen la experiencia del usuario y la eficiencia operativa. Estoy comprometido a seguir creciendo profesionalmente y aplicar mis habilidades para enfrentar nuevos desafíos en el ámbito del desarrollo de software y la gestión de contenido en línea.",
+}
