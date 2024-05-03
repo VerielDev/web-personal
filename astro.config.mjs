1f43b14fc2b8301bad33eff83a0e5593c505953d
samuraiOrDev <vm.ordiales.garcia@gmail.com>
2024-05-03 15:12:24 +0200

    Finalización etapa inicial blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from "astro/config"
 
+import mdx from "@astrojs/mdx"
+
+import node from "@astrojs/node"
+
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+	integrations: [mdx()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+})
