e42d28509430e7a998cded56f4628ba5414c7141
samuraiOrDev <vm.ordiales.garcia@gmail.com>
2024-05-03 17:56:56 +0200

    Finalziación etapa 2 blog

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,49 +1,49 @@
 export const $ = <T extends HTMLElement>(
 	selector: string,
 	context: Document | HTMLElement = document,
-) => context.querySelector<T>(selector)
+) => context.querySelector<T>(selector);
 
 export const $$ = <T extends HTMLElement>(
 	selector: string,
 	context: Document | HTMLElement = document,
-) => context.querySelectorAll<T>(selector)
+) => context.querySelectorAll<T>(selector);
 
 const animationAppear = (selector: string) => {
-	const element = $(selector)
+	const element = $(selector);
 	const observer = new IntersectionObserver(
 		(entries) => {
 			entries.forEach((entry) => {
 				if (entry.isIntersecting) {
-					entry.target.classList.add("appear")
+					entry.target.classList.add("appear");
 				} else {
 					entry.target.classList.remove("appear");
-					(entry.target as HTMLElement).style.opacity = "0"
+					(entry.target as HTMLElement).style.opacity = "0";
 				}
-			})
+			});
 		},
 		{ threshold: 0.5 },
-	)
+	);
 
-	observer.observe(element as HTMLElement)
-}
+	observer.observe(element as HTMLElement);
+};
 
 export const animationAppearElements = (selector: string) => {
-	const element = $$(selector)
+	const element = $$(selector);
 	const observer = new IntersectionObserver(
 		(entries) => {
 			entries.forEach((entry) => {
 				if (entry.isIntersecting) {
-					entry.target.classList.add("appear")
+					entry.target.classList.add("appear");
 				} else {
 					entry.target.classList.remove("appear");
-					(entry.target as HTMLElement).style.opacity = "0"
+					(entry.target as HTMLElement).style.opacity = "0";
 				}
-			})
+			});
 		},
 		{ threshold: 0.5 },
-	)
+	);
 
-	element.forEach((el) => observer.observe(el))
-}
+	element.forEach((el) => observer.observe(el));
+};
 
-export default animationAppear
+export default animationAppear;
