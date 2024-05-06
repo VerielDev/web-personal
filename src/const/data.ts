8e1312f1fb392520a3bf2b940f8c002695266bda
samuraiOrDev <vm.ordiales.garcia@gmail.com>
2024-05-06 20:07:53 +0200

    Finalziación 6 de mayo de 2024

--- src/const/data.ts
+++ src/const/data.ts
@@ -3,14 +3,14 @@ export const textHeroSection = {
 		"Hello I'am <span>Víctor Manuel</span> Ordiales García. <br> <span>Full Stack Developer</span>.",
 	description:
 		"Desarrollador Full Stack con experiencia en tecnologías como React, Node, Express, MongoDB, entre otras. Apasionado por la tecnología y la programación.",
-}
+};
 
 export const skillsSection = {
 	title: "My <span>Skills</span>",
-}
+};
 export const experienceSection = {
 	title: "My <span>Experience</span>",
-}
+};
 export const experiences = [
 	{
 		title: "Desarrollador OWCS",
@@ -33,22 +33,99 @@ export const experiences = [
 		description:
 			"Algoritmos predictivos y análisis de datos dentro el modelo 3D.",
 	},
-]
+];
 
 export const aboutSection = {
 	title: "About <span>Me</span>",
 	description:
 		"Graduado en Física y desarrollador full stack con habilidades sólidas en JavaScript (JS), TypeScript (TS), Java y sistemas de gestión de contenido como Oracle WebCenter Sites (OWCS).<br/> <br/>Mi formación en Física me ha proporcionado una mentalidad analítica y resolutiva, mientras que mi experiencia en desarrollo me permite crear soluciones técnicas eficientes y escalables. <br/> <br/>He trabajado en proyectos desafiantes que requieren un enfoque meticuloso y estratégico para la implementación de sistemas de gestión de contenido que optimicen la experiencia del usuario y la eficiencia operativa. Estoy comprometido a seguir creciendo profesionalmente y aplicar mis habilidades para enfrentar nuevos desafíos en el ámbito del desarrollo de software y la gestión de contenido en línea.",
-}
+};
 
 export const projectsSection = {
 	title: "My <span>Projects</span>",
-}
+};
 
 export const testimonialSection = {
 	title: "My <span>Testimonial</span>",
-}
+};
 export const formContactSection = {
 	title: "My <span>Contact</span>",
 	subTitle: "<span>Let's</span> talk <span>for Something special</span>",
-}
+};
+
+export const skillsJS = [
+	"<span>JavaScript (JS)</span> es un lenguaje de programación ampliamente utilizado tanto en el desarrollo web como en otros ámbitos. Inicialmente creado para agregar interactividad a las páginas web, JS ha evolucionado hasta convertirse en un lenguaje de programación versátil y poderoso que se ejecuta en el navegador del usuario. Es un componente fundamental de la web moderna y se utiliza para una amplia gama de aplicaciones, desde la creación de páginas web dinámicas hasta el desarrollo de aplicaciones web complejas y juegos en línea.",
+	"En mi día a día como desarrollador, JavaScript se ha convertido en el pilar fundamental de mi trabajo. Tanto en el ámbito del cliente com del servidor.",
+	"Dentro del contexto de mi trabajo, me encuentro inmerso en entornos que hacen uso de tecnologías como <span>Oracle WebCenter Sites (OWCS)</span> y <span>Spring Framework</span>. En este escenario, JavaScript se erige como una herramienta indispensable, permitiéndome interactuar con los elementos de la interfaz de usuario de forma ágil y eficiente, así como también facilitando la comunicación con el servidor para la manipulación de datos y la gestión de eventos.",
+	"A lo largo de mi trayectoria y formación, he tenido el privilegio de adentrarme en una variada selección de bibliotecas y herramientas dentro del vasto ecosistema de JavaScript. Entre estas, destaca <span>jQuery</span>, una biblioteca que ha mantenido su relevancia como un pilar del desarrollo web a lo largo de los años. Su sintaxis simplificada y su capacidad de implementación en prácticamente cualquier navegador actual son aspectos fundamentales que han enriquecido y agilizado mi labor actual.",
+	"Me identifico como un desarrollador con una amplia experiencia y un dominio sólido de JavaScript. Si por mí fuera, lo emplearía para todo jajaja. Aunque, últimamente, he estado optando por <span>TypeScript</span> en mis proyectos personales. El tipado estático que ofrece esta herramienta ha sido de gran ayuda, brindando mayor estructura y claridad a mi código.",
+];
+export const skillsHtml = [
+	"<span>HTML</span> es el lenguaje de marcado estándar utilizado para crear y estructurar páginas web. Es un componente fundamental de la web moderna y se utiliza para definir la estructura y el contenido de una página web. HTML se compone de una serie de elementos que permiten a los desarrolladores web crear páginas web interactivas y atractivas.",
+	"En mi día a día como desarrollador, HTML es una herramienta esencial que utilizo para definir la estructura de las páginas web y organizar el contenido de forma clara y coherente.",
+	"Dentro del contexto de trabajo, me encuentro inmerso en entornos que hacen uso de tecnologías como <span>Oracle WebCenter Sites (OWCS)</span> y <span>Spring Framework</span>. En este escenario, usado motores de plantillas como <span>JSP</span> y <span>Thymeleaf</span> para la generación de contenido dinámico en el servidor.",
+	"A lo largo de mi trayectoria y formación, me he familiarizado con una gran variedad de bliblotecas/frameworks que usan HTML y lo adaptan a sus necesidades. Cómo puede ser el caso de React, que utiliza JSX (js + html),  o template engines como <span>handlebar</span>s.",
+	"Me identifico como un desarrollador con una amplia experiencia y un dominio sólido de HTML. Aunque en la parte de accesibilidad, SEO y semántica debería profundizar mucho más para conseguir un conocimiento aún más profundo.",
+];
+export const skillsCss = [];
+
+export const itemsJS = [
+	{
+		value: 75,
+		title: "Tipado Dinámico",
+	},
+	{
+		value: 80,
+		title: "High Order Function",
+	},
+	{
+		value: 85,
+		title: "Clousures",
+	},
+	{
+		value: 85,
+		title: "Prototype",
+	},
+	{
+		value: 90,
+		title: "Async/Await",
+	},
+	{ value: 80, title: "Promises" },
+	{ value: 75, title: "Arrow Function" },
+	{ value: 70, title: "Events" },
+	{ value: 75, title: "Class" },
+];
+export const itemsHtml = [
+	{
+		value: 60,
+		title: "Semántica",
+	},
+	{
+		value: 70,
+		title: "Formulario",
+	},
+	{
+		value: 80,
+		title: "Multimedia",
+	},
+	{
+		value: 70,
+		title: "APIs",
+	},
+	{
+		value: 80,
+		title: "Etiquetas",
+	},
+	{
+		value: 75,
+		title: "SEO",
+	},
+	{
+		value: 70,
+		title: "Accesibilidad",
+	},
+	{
+		value: 70,
+		title: "Estructura",
+	},
+];
